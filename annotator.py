01cf36d34ad26580a54b30990bfe07d0ce8598bf
Siamul Karim Khan <siamulkarim@gmail.com>
2021-09-20 02:47:34 -0400

    Added enlarging, reducing and CLAHE

--- annotator.py
+++ annotator.py
@@ -1,12 +1,13 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 import os
-from PIL import ImageTk,Image,ImageDraw
+from PIL import ImageTk,Image,ImageDraw, ImageOps
 import numpy as np
 from scipy import optimize
 import math
 import json
-from tkinter import messagebox
+import cv2
 
 root = Tk()
 
@@ -21,7 +22,7 @@ current_img = None
 image_index = 0
 folder_path = ''
 
-points = {}  
+points = {}  #index 0 is pupil and 1 is iris
 points['pupil'] = []
 points['iris'] = []
 points_index = 'pupil'
@@ -29,6 +30,8 @@ delete_bool=False
 delete_dist_limit = 42
 current_img = None
 image_name_only = []
+clahe_bool = False
+image_mult = 1
 
 p_x, p_y, p_r, i_x, i_y, i_r = 0, 0, 0, 0, 0, 0
 
@@ -57,14 +60,30 @@ def leastsq_circle(x, y):
     return xc, yc, R, residu
 
 def create_image_with_points():
-    global images, image_index, points, p_x, p_y, p_r, i_x, i_y, i_r
+    global images, image_index, points, p_x, p_y, p_r, i_x, i_y, i_r, clahe_bool, image_mult
     img = Image.open(images[image_index]).convert('RGB')
+    if clahe_bool:
+        print("CLAHE!")
+        img_gray = img.convert('L')
+        #img = ImageOps.autocontrast(img_gray).convert('RGB')
+        
+        img_np = np.asarray(img_gray)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        img_np = clahe.apply(img_np)
+        img = Image.fromarray(img_np).convert('RGB')
+
+    width, height = img.size
+    width = width * image_mult
+    height = height * image_mult
+    img = img.resize((width, height))    
     draw = ImageDraw.Draw(img)
 
     for point in points['pupil']:
+        point = [image_mult * coordv for coordv in point]
         draw.ellipse((point[0]-2, point[1]-2, point[0]+2, point[1]+2), fill='red')
 
     for point in points['iris']:
+        point = [image_mult * coordv for coordv in point]
         draw.ellipse((point[0]-2, point[1]-2, point[0]+2, point[1]+2), fill='blue')
 
     if len(points['pupil']) >= 3:
@@ -73,7 +92,8 @@ def create_image_with_points():
         for point in points['pupil']:
             pupil_x_points.append(point[0])
             pupil_y_points.append(point[1])
-        p_x, p_y, p_r, p_res = leastsq_circle(pupil_x_points, pupil_y_points)
+        p_x, p_y, p_r, p_res = [retval * image_mult for retval in leastsq_circle(pupil_x_points, pupil_y_points)]
+
         draw.ellipse((p_x-p_r, p_y-p_r, p_x+p_r, p_y+p_r), outline='red')
         
     if len(points['iris']) >= 3:
@@ -82,13 +102,21 @@ def create_image_with_points():
         for point in points['iris']:
             iris_x_points.append(point[0])
             iris_y_points.append(point[1])
-        i_x, i_y, i_r, i_res = leastsq_circle(iris_x_points, iris_y_points)
+        i_x, i_y, i_r, i_res = [retval * image_mult for retval in leastsq_circle(iris_x_points, iris_y_points)]
         draw.ellipse((i_x-i_r, i_y-i_r, i_x+i_r, i_y+i_r), outline='blue')
 
     return ImageTk.PhotoImage(img)
 
 def select_image_folder():
-    global images, image_index, previous_image_button, next_image_button, pupil_button, iris_button, current_img, canvas, delete_button, image_name_only, folder_path, points
+    global images, image_index, previous_image_button, next_image_button, pupil_button, iris_button, \
+                    current_img, canvas, delete_button, image_name_only, folder_path, points
+    if len(images) != 0:    
+        data_file_prev = os.path.join(folder_path, image_name_only[image_index].split('.')[0] + '.json')
+        data = {}
+        data['points'] = points
+        data['pupil_xyr'] = [p_x, p_y, p_r]
+        data['iris_xyr'] = [i_x, i_y, i_r]
+        json.dump( data, open( data_file_prev, 'w' ) )
     images = []
     image_index = 0
     filename = filedialog.askdirectory()
@@ -114,6 +142,9 @@ def select_image_folder():
     iris_button['state']='normal'
     delete_button['state']='normal'
     clear_button['state']='normal'
+    enlarge_button['state']='normal'
+    reduce_button['state']='normal'
+    clahe_button['state']='normal'
     
     #canvas['state']='normal'
 
@@ -188,9 +219,9 @@ def delete_point():
 
 def callback(event):
     #print("clicked at", event.x, event.y)
-    global current_img, points, points_index, delete_bool
-    x = int(event.x)
-    y = int(event.y)
+    global current_img, points, points_index, delete_bool, image_mult
+    x = int(event.x / image_mult) 
+    y = int(event.y / image_mult)
     if delete_bool==False:
         points[points_index].append([x,y])
     else:
@@ -262,7 +293,36 @@ def on_closing():
         except Exception as e:
             pass
         root.destroy()
-    
+
+def apply_CLAHE():
+    global clahe_bool, canvas, current_img
+    if clahe_var.get() == 1:
+        clahe_bool = True
+    else:
+        clahe_bool = False
+    print("CLAHE bool is now: ", str(clahe_bool))
+    current_img = create_image_with_points()
+    canvas.config(width = current_img.width(), height = current_img.height())
+    canvas.create_image(0, 0, anchor=NW, image=current_img)
+        
+def enlarge_image():
+    global image_mult, canvas, current_img
+    if image_mult < 10:
+        image_mult += 1
+        print("Image Multiplier: ", image_mult)
+    current_img = create_image_with_points()
+    canvas.config(width = current_img.width(), height = current_img.height())
+    canvas.create_image(0, 0, anchor=NW, image=current_img)
+
+def reduce_image():
+    global image_mult, canvas, current_img
+    if image_mult > 1:
+        image_mult -= 1
+        print("Image Multiplier: ", image_mult)
+    current_img = create_image_with_points()
+    canvas.config(width = current_img.width(), height = current_img.height())
+    canvas.create_image(0, 0, anchor=NW, image=current_img)
+
 
 select_image_folder_button = Button(root, text="Select Image Folder", command=select_image_folder)
 previous_image_button = Button(root, text="Previous Image", state=DISABLED, command=previous_image)
@@ -271,6 +331,10 @@ pupil_button = Button(root, text ="Set pupil circle", state=DISABLED, relief=SUN
 iris_button = Button(root, text="Set iris circle", state=DISABLED, relief=RAISED, command=select_iris_list)
 delete_button = Button(root, text="Delete point", state=DISABLED, relief=RAISED, command=delete_point)
 clear_button = Button(root, text="Clear all points", state=DISABLED, command=clear_all_points)
+enlarge_button = Button(root, text="Enlarge Image",  state=DISABLED, command=enlarge_image)
+reduce_button = Button(root, text="Reduce Image", state=DISABLED, command=reduce_image)
+clahe_var = IntVar()
+clahe_button = Checkbutton(root, text='CLAHE',variable=clahe_var, state=DISABLED, onvalue=1, offvalue=0, command=apply_CLAHE)
 select_image_folder_button.grid(row=0, column=0)
 previous_image_button.grid(row=0, column=1)
 next_image_button.grid(row=0, column=2)
@@ -278,9 +342,12 @@ pupil_button.grid(row=0, column=3)
 iris_button.grid(row=0, column=4)
 delete_button.grid(row=0, column=5)
 clear_button.grid(row=0, column=6)
+clahe_button.grid(row=0, column=7)
+enlarge_button.grid(row=0, column=8)
+reduce_button.grid(row=0, column=9)
 canvas = Canvas(root)
 canvas.bind("<Button-1>", callback)
-canvas.grid(row=2, columnspan=7)
+canvas.grid(row=2, columnspan=10)
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
