731ab11615a94fe19f8780fcec332888a9da96c0
Siamul Karim Khan <siamulkarim@gmail.com>
2021-09-20 21:27:30 -0400

    Add files via upload

--- annotator.py
+++ annotator.py
@@ -31,7 +31,7 @@ delete_dist_limit = 42
 current_img = None
 image_name_only = []
 clahe_bool = False
-image_mult = 1
+image_mult = 10
 
 p_x, p_y, p_r, i_x, i_y, i_r = 0, 0, 0, 0, 0, 0
 
@@ -75,15 +75,15 @@ def create_image_with_points():
     width, height = img.size
     width = width * image_mult
     height = height * image_mult
-    img = img.resize((width, height))    
+    img = img.resize((width, height), Image.BICUBIC)    
     draw = ImageDraw.Draw(img)
 
     for point in points['pupil']:
-        point = [image_mult * coordv for coordv in point]
+        #point = [image_mult * coordv for coordv in point]
         draw.ellipse((point[0]-2, point[1]-2, point[0]+2, point[1]+2), fill='red')
 
     for point in points['iris']:
-        point = [image_mult * coordv for coordv in point]
+        #point = [image_mult * coordv for coordv in point]
         draw.ellipse((point[0]-2, point[1]-2, point[0]+2, point[1]+2), fill='blue')
 
     if len(points['pupil']) >= 3:
@@ -92,8 +92,7 @@ def create_image_with_points():
         for point in points['pupil']:
             pupil_x_points.append(point[0])
             pupil_y_points.append(point[1])
-        p_x, p_y, p_r, p_res = [retval * image_mult for retval in leastsq_circle(pupil_x_points, pupil_y_points)]
-
+        p_x, p_y, p_r, p_res = leastsq_circle(pupil_x_points, pupil_y_points)
         draw.ellipse((p_x-p_r, p_y-p_r, p_x+p_r, p_y+p_r), outline='red')
         
     if len(points['iris']) >= 3:
@@ -102,20 +101,20 @@ def create_image_with_points():
         for point in points['iris']:
             iris_x_points.append(point[0])
             iris_y_points.append(point[1])
-        i_x, i_y, i_r, i_res = [retval * image_mult for retval in leastsq_circle(iris_x_points, iris_y_points)]
+        i_x, i_y, i_r, i_res = leastsq_circle(iris_x_points, iris_y_points)
         draw.ellipse((i_x-i_r, i_y-i_r, i_x+i_r, i_y+i_r), outline='blue')
 
     return ImageTk.PhotoImage(img)
 
 def select_image_folder():
     global images, image_index, previous_image_button, next_image_button, pupil_button, iris_button, \
-                    current_img, canvas, delete_button, image_name_only, folder_path, points
+                    current_img, canvas, delete_button, image_name_only, folder_path, image_mult
     if len(images) != 0:    
         data_file_prev = os.path.join(folder_path, image_name_only[image_index].split('.')[0] + '.json')
+        global points, p_x, p_y, p_r, i_x, i_y, i_r
         data = {}
         data['points'] = points
-        data['pupil_xyr'] = [p_x, p_y, p_r]
-        data['iris_xyr'] = [i_x, i_y, i_r]
+        data['image_mult'] = image_mult
         json.dump( data, open( data_file_prev, 'w' ) )
     images = []
     image_index = 0
@@ -125,7 +124,7 @@ def select_image_folder():
         if imagename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
             images.append(os.path.join(folder_path, imagename))
             image_name_only.append(imagename)
-    Label(root, text=images[image_index]).grid(row=1, columnspan=7)
+    Label(root, text=images[image_index]).grid(row=1, columnspan=10)
     points['pupil'] = []
     points['iris'] = []
     data_file = os.path.join(folder_path, image_name_only[image_index].split('.')[0] + '.json')
@@ -133,6 +132,7 @@ def select_image_folder():
     if os.path.exists(data_file):
         data = json.load(open(data_file))
         points = data['points']
+        image_mult = data['image_mult']
     current_img = create_image_with_points()
     canvas.config(width = current_img.width(), height = current_img.height())
     canvas.create_image(0, 0, anchor=NW, image=current_img)
@@ -142,20 +142,21 @@ def select_image_folder():
     iris_button['state']='normal'
     delete_button['state']='normal'
     clear_button['state']='normal'
-    enlarge_button['state']='normal'
-    reduce_button['state']='normal'
+    #enlarge_button['state']='normal'
+    #reduce_button['state']='normal'
     clahe_button['state']='normal'
     
     #canvas['state']='normal'
 
 def previous_image():
-    global images, image_index, current_img, canvas, points
+    global images, image_index, current_img, canvas, points, image_mult
     if image_index > 0:
         data_file_prev = os.path.join(folder_path, image_name_only[image_index].split('.')[0] + '.json')
         data = {}
         data['points'] = points
         data['pupil_xyr'] = [p_x, p_y, p_r]
         data['iris_xyr'] = [i_x, i_y, i_r]
+        data['image_mult'] = image_mult
         json.dump( data, open( data_file_prev, 'w' ) )
         image_index -= 1
         Label(root, text=images[image_index]).grid(row=1, columnspan=7)
@@ -165,20 +166,22 @@ def previous_image():
         if os.path.exists(data_file):
             data = json.load(open(data_file))
             points = data['points']
+            image_mult = data['image_mult']
         
         current_img = create_image_with_points()
         canvas.config(width = current_img.width(), height = current_img.height())
         canvas.create_image(0, 0, anchor=NW, image=current_img)
 
 def next_image():
-    global images, image_index, current_img, canvas, points, p_x, p_y, p_r, i_x, i_y, i_r
+    global images, image_index, current_img, canvas, points, p_x, p_y, p_r, i_x, i_y, i_r, image_mult
     if image_index < (len(images) - 1):
         data_file_prev = os.path.join(folder_path, image_name_only[image_index].split('.')[0] + '.json')
         data = {}
         data['points'] = points
         data['pupil_xyr'] = [p_x, p_y, p_r]
         data['iris_xyr'] = [i_x, i_y, i_r]
-        json.dump( data, open( data_file_prev, 'w' ) )
+        data['image_mult'] = image_mult
+        json.dump(data, open( data_file_prev, 'w' ) )
         image_index += 1
         Label(root, text=images[image_index]).grid(row=1, columnspan=7)
         points['pupil'] = []
@@ -187,6 +190,8 @@ def next_image():
         if os.path.exists(data_file):
             data = json.load(open(data_file))
             points = data['points']
+            image_mult = data['image_mult']
+
         current_img = create_image_with_points()
         canvas.config(width = current_img.width(), height = current_img.height())
         canvas.create_image(0, 0, anchor=NW, image=current_img)
@@ -220,8 +225,8 @@ def delete_point():
 def callback(event):
     #print("clicked at", event.x, event.y)
     global current_img, points, points_index, delete_bool, image_mult
-    x = int(event.x / image_mult) 
-    y = int(event.y / image_mult)
+    x = int(event.x) 
+    y = int(event.y)
     if delete_bool==False:
         points[points_index].append([x,y])
     else:
@@ -331,8 +336,8 @@ pupil_button = Button(root, text ="Set pupil circle", state=DISABLED, relief=SUN
 iris_button = Button(root, text="Set iris circle", state=DISABLED, relief=RAISED, command=select_iris_list)
 delete_button = Button(root, text="Delete point", state=DISABLED, relief=RAISED, command=delete_point)
 clear_button = Button(root, text="Clear all points", state=DISABLED, command=clear_all_points)
-enlarge_button = Button(root, text="Enlarge Image",  state=DISABLED, command=enlarge_image)
-reduce_button = Button(root, text="Reduce Image", state=DISABLED, command=reduce_image)
+#enlarge_button = Button(root, text="Enlarge Image",  state=DISABLED, command=enlarge_image)
+#reduce_button = Button(root, text="Reduce Image", state=DISABLED, command=reduce_image)
 clahe_var = IntVar()
 clahe_button = Checkbutton(root, text='CLAHE',variable=clahe_var, state=DISABLED, onvalue=1, offvalue=0, command=apply_CLAHE)
 select_image_folder_button.grid(row=0, column=0)
@@ -343,8 +348,8 @@ iris_button.grid(row=0, column=4)
 delete_button.grid(row=0, column=5)
 clear_button.grid(row=0, column=6)
 clahe_button.grid(row=0, column=7)
-enlarge_button.grid(row=0, column=8)
-reduce_button.grid(row=0, column=9)
+#enlarge_button.grid(row=0, column=8)
+#reduce_button.grid(row=0, column=9)
 canvas = Canvas(root)
 canvas.bind("<Button-1>", callback)
 canvas.grid(row=2, columnspan=10)
